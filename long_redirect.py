eca429c66d1ffe52f74470be17958ea85381ff60
badzhelidze <badjelidze_ilya@mail.ru>
2022-02-04 22:34:29 +0300

    Файл "long_redirect.py" с кодом по работе с конструкцией history.

--- long_redirect.py
+++ long_redirect.py
@@ -1,8 +1,6 @@
 import requests
 
 response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
-#redirect_history = response.history
-#final_url = response.url
 
 print(response.history)
 print(response.history[0].url)
