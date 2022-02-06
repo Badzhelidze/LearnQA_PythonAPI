990291b18a3ac8800786e18b13c2ae83f4bdded2
badzhelidze <badjelidze_ilya@mail.ru>
2022-02-06 13:31:47 +0300

    Файл "Ext8Tokens.py" с кодом по работе с токенами и задачами.

---
+++ Ext8Tokens.py
@@ -0,0 +1,40 @@
+import json
+import time
+import requests
+
+# Script #1 - создаём задачу.
+response1 = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job")
+content1 = json.loads(response1.content)
+token = (content1["token"])
+secs = (content1["seconds"])
+payload = {"token": token}
+print(response1.text)
+
+
+# Script #2 - делаем один запрос с token ДО того, как задача готова. Убеждаемся в правильности поля status.
+response2 = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params=payload)
+content2 = json.loads(response2.content)
+status = (content2["status"])
+if status == "Job is NOT ready":
+    print(f'При запросе с token ДО того как задача готова получаем статус = "{status}"')
+
+
+# Script #3 - ждем нужное количество секунд.
+print('                  ||')
+print('                  ||')
+print('                  \/')
+print(f'Ждем нужное кол-во секунд: {secs} сек.')
+time.sleep(secs)
+
+
+# Script #4 - делаем один запрос c token ПОСЛЕ того, как задача готова. Убеждаемся в правильности поля status и наличии поля result.
+response3 = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params=payload)
+content3 = json.loads(response3.content)
+actual_status = (content3["status"])
+result_field = (content3["result"])
+try:
+    assert actual_status == "Job is ready"
+    assert result_field is not None
+    print(f'Получен статус = {actual_status}. Задача выполнена успешно!')
+except AssertionError:
+    print("Что-то пошло не так. Задача до сих пор в процессе выполнения!")
