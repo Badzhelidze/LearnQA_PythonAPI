60a097a9b363ebbf70169c6452bc20bd17263b24
badzhelidze <badjelidze_ilya@mail.ru>
2022-02-11 17:33:04 +0300

    Файл "Ext11CookieTestRequest.py" с кодом по тестированию запроса на метод cookie.

--- Ext11CookieTestRequest.py
+++ Ext11CookieTestRequest.py
@@ -3,7 +3,7 @@ import requests
 
 class Test_Cookie:
     def test_cookie_request(self):
-        response = requests.get("https://playground.learnqa.ru/api/homework_cookie", cookies={"HomeWork": "12345"})
+        response = requests.get("https://playground.learnqa.ru/api/homework_cookie")
         print(
             f'Метод GET "/homework_cookie" возвращает COOKIE = {dict(response.cookies)}, значение которой = "{response.cookies.get("HomeWork")}"')
         assert response.cookies.get('HomeWork') == "hw_value", "Ошибка! МЫ не получаем нужную COOKIE ='hw_value'!"
